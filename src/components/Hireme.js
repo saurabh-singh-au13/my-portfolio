4219a0c41270b8d7ee1186e0228284c5e913fbdd
Saurabh Singh <saurabhsingh@classicinformatics.com>
2023-03-19 21:30:30 +0530

    changes

---
+++ src/components/Hireme.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Mynav.css";
+const Hireme = () => {
+  return (
+    <>
+      <div className="cv1">
+          <div className="cert">
+            <div className="c1">
+              <div className="certi">Hire Me 😁</div>
+            </div>
+            <lord-icon style={{ width:"200px", height: "200px"}}
+              src="https://cdn.lordicon.com/zpxybbhl.json"
+              trigger="hover"
+            ></lord-icon>
+          </div>
+        </div>
+
+     
+    </>
+  );
+};
+
+export default Hireme;
