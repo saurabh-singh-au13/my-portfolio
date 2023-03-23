5de9994cade02b24e3196471e163866aa54c3bbe
Saurabh Kumar Singh <s.rajvansh.1996@gmail.com>
2023-03-24 00:14:21 +0530

    added whatsapp chat

--- src/components/Mynav.js
+++ src/components/Mynav.js
@@ -6,21 +6,27 @@ import Myport from "./Myport";
 import Myprofile from "./Myprofile";
 import Myskills from "./Myskills";
 
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+
 const Mynav = () => {
+  const linkCV = "https://drive.google.com/file/d/1-KNAnaHCL5j4WMitKxTbB59M90hL7mo5/view?usp=sharing";
+  const linkProject = "https://disneyplus-clone-1f559.web.app/";
   return (
     <>
       <a
-        href="https://drive.google.com/file/d/1-KNAnaHCL5j4WMitKxTbB59M90hL7mo5/view?usp=sharing"
+        href={linkCV} download
         target="_new"
+        
       >
         <div className="c1">
-          <div className="resume">My CV</div>
+          <div className="resume"> My CV</div>
         </div>
+       
       </a>
-      <a href="https://disneyplus-clone-1f559.web.app/" target="_new">
+      <a href={linkProject} target="_new">
         <div className="c1">
 
-          <div className="resume">Project</div>
+          <div className="resume" title="Disney+ Hotstar">Project</div>
         </div>
       </a>
       <div className="container">
@@ -71,6 +77,19 @@ const Mynav = () => {
             </a>
           </div>
         </div>
+      </div>
+      <div>
+        <a href="https://wa.me/7291089688" 
+      //  
+        target="_blank"
+       
+        >
+          {/* <i className="fa fa-whatsapp whatsapp-icon"></i> */}
+          <FloatingWhatsApp
+          style={{ disply:"none"}}
+          />
+        </a>
+
       </div>
     </>
   );
